2994f9135f506dfee751c83d1f2b4d111ade699a
Fumiya Sakai <just1factory@gmail.com>
2018-11-09 19:57:21 +0900

    Initial Commit

---
+++ ScrollAnimationShowcase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollAnimationShowcase
+//
+//  Created by 酒井文也 on 2018/11/09.
+//  Copyright © 2018 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
