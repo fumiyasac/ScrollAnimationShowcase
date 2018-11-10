0ca8ff3ef7567e2beb6979f2dc994f3420f2f91f
Fumiya Sakai <just1factory@gmail.com>
2018-11-10 10:14:47 +0900

    記事表示用のViewControllerを作成

---
+++ ScrollAnimationShowcase/ViewController/ArticleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleViewController.swift
+//  ScrollAnimationShowcase
+//
+//  Created by 酒井文也 on 2018/11/10.
+//  Copyright © 2018 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
