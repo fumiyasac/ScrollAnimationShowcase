4ad075e97b2072aba0d47e2e122604c487f5f066
Fumiya Sakai <just1factory@gmail.com>
2018-11-19 23:33:11 +0900

    必要そうなものを追加した

---
+++ ScrollAnimationShowcase/View/CategoryScrollContentsViewCell/CategoryScrollContentsViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryScrollContentsViewCell.swift
+//  ScrollAnimationShowcase
+//
+//  Created by 酒井文也 on 2018/11/16.
+//  Copyright © 2018 酒井文也. All rights reserved.
+//
+
+import UIKit
+
+final class CategoryScrollContentsViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- ScrollAnimationShowcase/ViewController/Article/ScrollContents/CategoryScrollContentsViewController.swift
+++ ScrollAnimationShowcase/ViewController/Article/ScrollContents/CategoryScrollContentsViewController.swift
@@ -10,17 +10,55 @@ import UIKit
 
 class CategoryScrollContentsViewController: UIViewController {
 
-    // MEMO: 後に実際のものに差し替える
+    @IBOutlet weak private var categoryScrollContentsCollectionView: UICollectionView!
 
+    // MEMO: 後に実際のものに差し替える
     @IBOutlet weak private var sampleTitleLabel: UILabel!
-    
+
+    // MARK: - Override
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupCategoryScrollContentsCollectionView()
     }
-    
+
     // MARK: - Function
 
     func setTitle(text: String) {
         sampleTitleLabel.text = "現在表示しているIndex値：\(text)"
+    }
+
+    // MARK: - Private Function
+
+    private func setupCategoryScrollContentsCollectionView() {
+        categoryScrollContentsCollectionView.delegate = self
+        categoryScrollContentsCollectionView.dataSource = self
+        categoryScrollContentsCollectionView.registerCustomCell(CategoryScrollContentsViewCell.self)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension CategoryScrollContentsViewController: UICollectionViewDelegate {}
+
+// MARK: - UICollectionViewDataSource
+
+extension CategoryScrollContentsViewController: UICollectionViewDataSource {
+
+    // 配置するセルの個数を設定する
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+
+    // 配置するセルの表示内容を設定する
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCustomCell(with: CategoryScrollContentsViewCell.self, indexPath: indexPath)
+        return cell
+    }
+
+    // セル押下時の処理内容を記載する
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
     }
 }
